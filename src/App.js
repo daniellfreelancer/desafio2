28b97aa3a90804efcdc13f5ad955d3957354318d
karla <daniellugofreelancer@gmail.com>
2022-01-11 10:37:31 -0300

    desafio2

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+function App() {
+  return (
+    
+    <div>
+    <nav className="navbar navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Marketplace Meats
+          </a>
+          <ul>
+              <li>Based Plants</li>
+              <li>Red Meat</li>
+              <li>White Meat</li>
+              <li>Sea Meat</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
+      
+
+
+  );
+
+  
+
+
+}
+
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,20 @@
+export const  menubar = () => {
+
+    return (
+        <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+            Bootstrap
+          </a>
+          <ul>
+              <li>menu 1</li>
+          </ul>
+        </div>
+      </nav>
+
+    )
+
+}
+
+export default menubar;
