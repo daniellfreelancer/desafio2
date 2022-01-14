aa00a2041674a1e7eb5da1da338591866910d99d
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-01-14 13:19:16 -0300

    Subida carpeta assets y compomente Navbar

---
+++ src/assets/Navbar.js
@@ -0,0 +1,21 @@
+
+function Navbar   () {
+    return <nav className="navbar navbar-light bg-light">
+      <div className="container">
+           <a className="navbar-brand" href="#">
+             Marketplace Meats
+           </a>
+           <ul>
+               <li>Based Plants</li>
+             <li>Red Meat</li>
+               <li>White Meat</li>
+               <li>Sea Meat</li>
+           </ul>
+      </div>
+    </nav>;
+    
+}
+
+export default Navbar;
+
+
