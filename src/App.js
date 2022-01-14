1d26f30196bf742cde15084d8a17415b4cb10ffb
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-01-14 13:17:40 -0300

    Import del compomente Navbar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
+// imports
 
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Navbar from './assets/Navbar';
 
 
 function App() {
   return (
+
+    <nav>
+    <Navbar />
+    </nav>
     
-    <div>
-    <nav className="navbar navbar-light bg-light">
-        <div className="container">
-          <a className="navbar-brand" href="#">
-            Marketplace Meats
-          </a>
-          <ul>
-              <li>Based Plants</li>
-              <li>Red Meat</li>
-              <li>White Meat</li>
-              <li>Sea Meat</li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-
-      
-
-
   );
-
-  
-
 
 }
 
